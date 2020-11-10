a9c1763a193104105b5f40b8d822ffef969f2287
Łukasz Królak <lukasz.krolak@gmail.com>
2020-11-10 22:13:23 +0100

    18.3 problem prop type

--- src/components/features/OrderOption/OrderOptionNumber.js
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -2,26 +2,26 @@ import React from 'react';
 import styles from './OrderOption.scss';
 import PropTypes from 'prop-types';
 
-
 const OrderOptionNumber = ({ currentValue, setOptionValue}) => (
   <div className={styles.number}>
-
+    {console.log('current',currentValue)}
     <input className={styles.inputSmall}
       type={'number'}
       value={currentValue}
       onChange={event => setOptionValue(event.currentTarget.value)}
-      limits={'min','max'}
+      
+      min="limits.min"
+      max="2"
     />
-
+    
   </div>
 );
 
 OrderOptionNumber.propTypes = {
   values:  PropTypes.object,
-  currentValue: PropTypes.number,
+  currentValue: PropTypes.string,
   setOptionValue: PropTypes.func,
   limits: PropTypes.object,
-  min:PropTypes.number,
 };
 
 export default OrderOptionNumber;
