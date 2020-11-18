5e8bd96735a59fe04b3bdfbdb71915b481fa6cdb
Łukasz Królak <lukasz.krolak@gmail.com>
2020-11-18 22:50:39 +0100

    19.2 test 2 fix

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -9,30 +9,27 @@ describe('Component TripSummary', () => {
     console.log(component.debug());
   });
   //test2
-  // it('should render correct src and alt', (src, alt) => {
-  //   const expectedName = 'name';
-  //   const expectedImage = 'image.jpg';
-  //   const component = shallow(<TripSummary image={expectedImage} name={expectedName} tags={[]}/>);
+  it('should render correct src and alt', () => {    
+    const expectedName = 'nnn';
+    const expectedImage = 'image.jpg';
+    const component = shallow(<TripSummary image={expectedImage} name={expectedName} tags={[]}/>);
     
-  //   const renderedName = component.find('.alt').prop([alt]).text(); 
-  //   // w tripSummary wystepuje klilka miejsc 
-  //   //gdzie jest przekazany props name stąd szukam po alt, i wciąż jest błąd
-  //   // renderedName="", a nie expectedName
-  //   expect(renderedName).toEqual(expectedName);
-  //   expect(component.find('img').prop([src])).toEqual(expectedImage);
-  // });
-  //test3
-  it('should render without crashing', () => {
-    const component = shallow(<TripSummary name='12' cost='50' tags={[]}/>);
-    expect(component).toBeTruthy(12);
-    expect(component).toBeTruthy(50);
-    console.log(component.debug());
+    const renderedName = component.find('img').prop('name').text(); 
+    expect(renderedName).toEqual(expectedName);
+    expect(component.find('img').prop('image')).toEqual(expectedImage);
   });
+  // //test3
+  // it('should render without crashing', () => {
+  //   const component = shallow(<TripSummary name='12' cost='50' tags={[]}/>);
+  //   expect(component).toBeTruthy(12);
+  //   expect(component).toBeTruthy(50);
+  //   console.log(component.debug());
+  // });
   
 
 
-  //test4 //   name: PropTypes.string.isRequired,
-  it('should throw error without required props', () => {
-    expect(() => shallow(<TripSummary />)).toThrow();
-  });
+  // //test4 //   name: PropTypes.string.isRequired,
+  // it('should throw error without required props', () => {
+  //   expect(() => shallow(<TripSummary />)).toThrow();
+//   });
 });
