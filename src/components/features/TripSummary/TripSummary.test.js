c5a6b1335bcb9e568715120f2717653779b6a8af
Łukasz Królak <lukasz.krolak@gmail.com>
2020-11-19 23:56:17 +0100

    19.2 tags

--- src/components/features/TripSummary/TripSummary.test.js
+++ src/components/features/TripSummary/TripSummary.test.js
@@ -14,23 +14,28 @@ describe('Component TripSummary', () => {
     const expectedImage = 'image.jpg';
     const component = shallow(<TripSummary image={expectedImage} name={expectedName} tags={[]}/>);
     
-    const renderedName = component.find('img').prop('name').text(); 
+    const renderedName = component.find('img').prop('name'); 
     expect(renderedName).toEqual(expectedName);
     expect(component.find('img').prop('image')).toEqual(expectedImage);
   });
   //test3
   it('should render without crashing', () => {
     const component = shallow(<TripSummary name={'nameABC'}  cost={'50'} days={3} tags={[]}/>);
-    //expect(component).toBeTruthy('aB');
     expect(component).toBeTruthy();
 
     console.log(component.debug());
   });
-  
-
-
   //test4 //  
   it('should throw error without required props', () => {
     expect(() => shallow(<TripSummary />)).toThrow();
   });
+  //test 5
+  // it('should render tags in correct sequence', () => {
+  //powinno renderowac w odpowiedniej kolejnosci,
+  // const expectedTags = tags={['one', 'two', 'three']};
+  //   const component = shallow(<TripSummary tags={['one', 'two', 'three']}/>);
+
+  //   expect(component.find('tags').at(0).prop('tags').foo).toEqual('one');
+  //oczekuj wyszykania 
+  // });
 });
